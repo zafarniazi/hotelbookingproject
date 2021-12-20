00b2757b1c985bd5c2435c5b3f97d13c341bcbd2
zafar <zafarkhan808045@gmail.com>
2021-12-20 17:28:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/booking", bookingsRoute);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Node JS Server Started`));
