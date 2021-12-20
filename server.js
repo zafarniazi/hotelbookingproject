8bbe02faf91d18df57a7e6c45b54403d3b8c84c8
zafar <zafarkhan808045@gmail.com>
2021-12-20 17:52:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bdconfig = require("./db");
-const path = require("path");
 const roomsRoute = require("./routes/roomRoutes");
 const usersRoute = require("./routes/userRoute");
 const bookingsRoute = require("./routes/bookingsRoute");
@@ -14,13 +13,6 @@ app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/booking", bookingsRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Node JS Server Started`));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
